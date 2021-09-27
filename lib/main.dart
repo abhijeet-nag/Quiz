ab690a7ccaa2c04fb96f7f7aa17ddc33436572db
abhijeet-nag <abhijeet.nag_cs18@gla.ac.in>
2021-09-27 11:14:16 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:quiz/pages/teacher/courses.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: HomePage(),
-      // home: Courses(),
+      home: Courses(),
     );
   }
 }
